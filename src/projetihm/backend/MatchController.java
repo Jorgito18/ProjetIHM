4cc8327a74708e12a0ee0fc8fd0d8a7342104907
Jorgito18 <34506874+Jorgito18@users.noreply.github.com>
2019-12-10 00:46:25 +0100

    Update MatchController.java

--- src/projetihm/backend/MatchController.java
+++ src/projetihm/backend/MatchController.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetihm.backend;
 
 import java.net.URL;
-import java.time.LocalTime;
 import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import javafx.fxml.FXML;
@@ -17,15 +9,8 @@ import javafx.fxml.Initializable;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Platform;
-import javafx.concurrent.Task;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.util.Duration;
 
 /**
  *
@@ -33,9 +18,6 @@ import javafx.util.Duration;
  */
 public class MatchController implements Initializable {
 
-    @FXML private VBox sideMenu;
-    @FXML private Font x1;
-    @FXML private Font x3;
     @FXML private Label minute;
     @FXML private Label seconde;
            
@@ -50,10 +32,15 @@ public class MatchController implements Initializable {
         executorService.scheduleAtFixedRate(new Runnable() {
         @Override
         public void run() {
-                for (int i = 0; i <= 60; i++) {
+                int j =0;
+                for (int i = 0; i < 60; i++) {
                     try {
                         Thread.sleep(1000);
-                        incrementMinutes(i);
+                        incrementSecondes(i);
+                        if(i == 59){
+                            j++;
+                            incrementMinutes(j);    
+                        }
                     } catch (InterruptedException ex) {
                         Logger.getLogger(MatchController.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -62,6 +49,15 @@ public class MatchController implements Initializable {
         }, 0, 1, TimeUnit.SECONDS);
     }
 
+    private void incrementSecondes(int i) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                seconde.setText(String.valueOf(i));
+            }
+        });
+    }
+    
     private void incrementMinutes(int i) {
         Platform.runLater(new Runnable() {
             @Override
