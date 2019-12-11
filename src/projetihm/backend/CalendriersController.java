c856153f91c376dfc11a7e6de394501bdb8acbbd
Jorgito18 <34506874+Jorgito18@users.noreply.github.com>
2019-12-11 16:52:04 +0100

    Add files via upload

---
+++ src/projetihm/backend/CalendriersController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetihm.backend;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import static projetihm.backend.MatchController.CLASEMENTS;
+import static projetihm.backend.MatchController.LOGIN;
+
+/**
+ * FXML Controller class
+ *
+ * @author Jorge Ara
+ */
+public class CalendriersController implements Initializable {
+    public static final String LOGIN = "/projetihm/frontend/Login.fxml";
+    public static final String CLASEMENTS = "/projetihm/frontend/Classements.fxml";
+    @FXML private ImageView retour;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    @FXML
+    public void openAsClassements() {
+        redirectFromCalendriers(CLASEMENTS);
+    }
+    
+    @FXML
+    public void redirectFromCalendriers(String windowPath) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource(windowPath));
+            fxmlLoader.load();
+
+            Parent parent = fxmlLoader.getRoot();
+            Stage stage = new Stage(StageStyle.DECORATED);
+            Stage mainStage = (Stage) retour.getScene().getWindow();
+            
+            stage.setTitle("Association Française d'Handball");
+            stage.setScene(new Scene(parent));
+            
+            stage.show();
+            mainStage.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    
+    public void showHelp(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Aide");
+        alert.setHeaderText("Calendriers");
+        String s ="Cette fenetre montre le calendrier des match des ligues de handball.";
+        alert.setContentText(s);
+        alert.show();
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("projetihm/images/lnh-logo_petit.png"));
+    }
+    
+}
